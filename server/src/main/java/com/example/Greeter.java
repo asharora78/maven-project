34177747bf007d55253cff1bb0e0d97bc325cecf
Ashwani Kumar <akumar118@sapient.com>
2017-06-05 12:40:26 +0530

    a

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,8 +12,11 @@ public class Greeter {
 
   }
 
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+  /**
+  * @param someone somone
+  * @return greeting greting 
+  */
+  public final String greet(String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
